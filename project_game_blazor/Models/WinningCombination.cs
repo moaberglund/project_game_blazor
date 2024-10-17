c243bf179b20d4e93cc74aa4b9f73be86cec745b
moaberglund <moa.21@hotmail.com>
2024-10-17 10:50:04 +0200

    klass för koll av vinnare

---
+++ project_game_blazor/Models/WinningCombination.cs
@@ -0,0 +1,20 @@
+﻿namespace project_game_blazor.Models
+{
+    public class WinningCombination
+    {
+
+        public int Square1 { get; }
+
+        public int Square2 { get; }
+
+        public int Square3 { get; }
+
+        //Konstruktor
+        public WinningCombination(int square1, int square2, int square3)
+        {
+            Square1 = square1;
+            Square2 = square2;
+            Square3 = square3;
+        }
+    }
+}
